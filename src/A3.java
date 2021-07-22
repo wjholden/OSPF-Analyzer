06b683f31501955d5b0757617ac0e5516ae878ab
William John Holden <wjholden@gmail.com>
2021-07-22 20:47:29 +0200

    neo4j.conf settings

--- src/A3.java
+++ src/A3.java
@@ -1,28 +1,80 @@
 import com.wjholden.ospf.Lsa;
+import org.neo4j.common.DependencyResolver;
+import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.configuration.SettingMigrators;
+import org.neo4j.configuration.connectors.BoltConnector;
+import org.neo4j.configuration.connectors.HttpConnector;
 import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
+import org.neo4j.exceptions.KernelException;
+import org.neo4j.graphalgo.BaseProc;
+import org.neo4j.graphalgo.catalog.GraphCreateProc;
+import org.neo4j.graphalgo.catalog.GraphListProc;
+import org.neo4j.graphalgo.pagerank.PageRankStreamProc;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.kernel.api.exceptions.Status;
+import org.neo4j.kernel.api.procedure.GlobalProcedures;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.snmp4j.security.AuthSHA;
 import org.snmp4j.security.SecurityProtocols;
 import org.soulwing.snmp.*;
-import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
+import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 
 public class A3 {
 
-    public static void main (String args[]) throws IOException {
+    public static void main (String args[]) throws IOException, KernelException {
         final Path databaseDirectory = Files.createTempDirectory(DEFAULT_DATABASE_NAME);
         System.out.println(databaseDirectory);
 
-        // create the database
-        final DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(databaseDirectory).build();
+        // This is an ugly hack to get around frustrations I have getting neo4j
+        // to obey the settings I have to specify in a neo4j.conf file.
+        final Path cfg = Files.createTempFile("neo4j", "conf");
+        try (BufferedWriter writer = Files.newBufferedWriter(cfg)) {
+            writer.write("dbms.connector.bolt.enabled=true");
+            writer.newLine();
+            writer.write("dbms.security.procedures.unrestricted=jwt.security.*,gds.*,apoc.*");
+            writer.newLine();
+            writer.write("dbms.security.procedures.whitelist=gds.*");
+            writer.newLine();
+        }
+
+        Map<String, String> settings = new HashMap<>();
+        settings.put(GraphDatabaseSettings.procedure_whitelist.name(), "*");
+        settings.put("dbms.security.procedures.unrestricted", "gds.*");
+        settings.put("dbms.security.procedures.whitelist", "gds.*");
+
+        // Create the database
+        final DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(databaseDirectory).
+                setConfig(BoltConnector.enabled, true).
+                setConfigRaw(settings).
+                build();
+        // You will be able to connect to neo4j://localhost:7687/neo4j with blank username/blank password.
+        // This is not mentioned at https://neo4j.com/docs/java-reference/current/java-embedded/bolt/#java-embedded-bolt.
         final GraphDatabaseService graphDb = managementService.database(DEFAULT_DATABASE_NAME);
         registerShutdownHook(managementService);
+
+        GlobalProcedures proceduresRegistry = ((GraphDatabaseAPI) graphDb)
+                .getDependencyResolver()
+                .resolveDependency(GlobalProcedures.class, DependencyResolver.SelectionStrategy.SINGLE);
+        List<Class<? extends BaseProc>> procsToRegister = List.of(
+                GraphListProc.class,
+                GraphCreateProc.class,
+                PageRankStreamProc.class
+        );
+        for (Class<?> procedureClass : procsToRegister) {
+            proceduresRegistry.registerProcedure(procedureClass);
+
+        }
 
         Mib mib = MibFactory.getInstance().newMib();
         mib.load("SNMPv2-MIB");
@@ -44,16 +96,20 @@ public class A3 {
             //SnmpWalker<VarbindCollection> walker = context.walk(1, "ospfLsdbAdvertisement");
 
             VarbindCollection row = walker.next().get();
-            System.out.println(row.getClass().getName());
             while (row != null) {
                 byte[] lsa = (byte[]) row.get("ospfLsdbAdvertisement").toObject();
-                //System.out.println(Arrays.toString(lsa));
-                System.out.println(Lsa.getInstance(lsa));
+                    //System.out.println(Lsa.getInstance(lsa));
                 row = walker.next().get();
                 i++;
             }
             System.out.println("Count: " + i);
+        } catch (TimeoutException ex) {
+            System.err.println("SNMP connection timed out");
         }
+
+        System.out.print("Press any key to exit...");
+        System.in.read();
+        System.exit(0);
     }
 
     private static void registerShutdownHook( final DatabaseManagementService managementService )
