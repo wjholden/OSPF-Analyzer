7aa9fc393deba476f08fa21685d169dc5899fca1
William John Holden <wjholden@gmail.com>
2021-07-22 23:00:07 +0200

    Bolt works to Neo4j Desktop

--- src/A3.java
+++ src/A3.java
@@ -1,29 +1,24 @@
 import com.wjholden.ospf.Lsa;
 import org.neo4j.common.DependencyResolver;
-import org.neo4j.configuration.GraphDatabaseSettings;
-import org.neo4j.configuration.SettingMigrators;
-import org.neo4j.configuration.connectors.BoltConnector;
-import org.neo4j.configuration.connectors.HttpConnector;
 import org.neo4j.dbms.api.DatabaseManagementService;
 import org.neo4j.dbms.api.DatabaseManagementServiceBuilder;
 import org.neo4j.exceptions.KernelException;
-import org.neo4j.graphalgo.BaseProc;
-import org.neo4j.graphalgo.catalog.GraphCreateProc;
-import org.neo4j.graphalgo.catalog.GraphListProc;
+import org.neo4j.graphalgo.betweenness.BetweennessCentralityStreamProc;
+import org.neo4j.graphalgo.centrality.ClosenessCentralityProc;
+import org.neo4j.graphalgo.functions.AsNodeFunc;
 import org.neo4j.graphalgo.pagerank.PageRankStreamProc;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.kernel.api.exceptions.Status;
 import org.neo4j.kernel.api.procedure.GlobalProcedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.snmp4j.security.AuthSHA;
 import org.snmp4j.security.SecurityProtocols;
 import org.soulwing.snmp.*;
 
-import java.io.*;
-import java.nio.charset.Charset;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,26 +31,14 @@ public class A3 {
         final Path databaseDirectory = Files.createTempDirectory(DEFAULT_DATABASE_NAME);
         System.out.println(databaseDirectory);
 
-        // This is an ugly hack to get around frustrations I have getting neo4j
-        // to obey the settings I have to specify in a neo4j.conf file.
-        final Path cfg = Files.createTempFile("neo4j", "conf");
-        try (BufferedWriter writer = Files.newBufferedWriter(cfg)) {
-            writer.write("dbms.connector.bolt.enabled=true");
-            writer.newLine();
-            writer.write("dbms.security.procedures.unrestricted=jwt.security.*,gds.*,apoc.*");
-            writer.newLine();
-            writer.write("dbms.security.procedures.whitelist=gds.*");
-            writer.newLine();
-        }
-
         Map<String, String> settings = new HashMap<>();
-        settings.put(GraphDatabaseSettings.procedure_whitelist.name(), "*");
-        settings.put("dbms.security.procedures.unrestricted", "gds.*");
+        settings.put("dbms.security.procedures.unrestricted", "jwt.security.*,gds.*,apoc.*");
         settings.put("dbms.security.procedures.whitelist", "gds.*");
+        settings.put("dbms.connector.bolt.enabled", "true");
+        settings.put("dbms.connector.http.enabled", "true");
 
         // Create the database
         final DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(databaseDirectory).
-                setConfig(BoltConnector.enabled, true).
                 setConfigRaw(settings).
                 build();
         // You will be able to connect to neo4j://localhost:7687/neo4j with blank username/blank password.
@@ -63,49 +46,41 @@ public class A3 {
         final GraphDatabaseService graphDb = managementService.database(DEFAULT_DATABASE_NAME);
         registerShutdownHook(managementService);
 
+        System.out.println("An embedded Neo4j graph database is running at: neo4j://localhost:7687/neo4j");
+        System.out.println("Connect to the database with Neo4j Desktop.");
+        System.out.println("Username = (blank)");
+        System.out.println("Password = (blank)");
+
+        // See https://github.com/neo4j/graph-data-science/issues/91,
+        // "How to install Graph Data Science (GDS) library embedded in Java applications".
+        // The instructions from Mats-SX are extremely helpful.
         GlobalProcedures proceduresRegistry = ((GraphDatabaseAPI) graphDb)
                 .getDependencyResolver()
                 .resolveDependency(GlobalProcedures.class, DependencyResolver.SelectionStrategy.SINGLE);
-        List<Class<? extends BaseProc>> procsToRegister = List.of(
-                GraphListProc.class,
-                GraphCreateProc.class,
-                PageRankStreamProc.class
-        );
+        Class[] procsToRegister = {
+                PageRankStreamProc.class,
+                ClosenessCentralityProc.class,
+                BetweennessCentralityStreamProc.class
+        };
+
+        // Cannot use forEach with checked exceptions.
         for (Class<?> procedureClass : procsToRegister) {
             proceduresRegistry.registerProcedure(procedureClass);
-
         }
 
-        Mib mib = MibFactory.getInstance().newMib();
-        mib.load("SNMPv2-MIB");
-        mib.load("IF-MIB");
-        mib.load("OSPF-MIB");
-
-        SecurityProtocols.getInstance().addAuthenticationProtocol(new AuthSHA());
-        SimpleSnmpV3Target target = new SimpleSnmpV3Target();
-        target.setAddress("192.168.0.1");
-        target.setSecurityName("admin");
-        target.setAuthType(SnmpV3Target.AuthType.SHA);
-        target.setPrivType(SnmpV3Target.PrivType.AES128);
-        target.setAuthPassphrase(System.getProperty("tnm4j.agent.auth.password", "cisco123"));
-        target.setPrivPassphrase(System.getProperty("tnm4j.agent.priv.password", "cisco123"));
-
-        try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
-            int i = 0;
-            SnmpWalker<VarbindCollection> walker = context.walk(1, "sysName", "ospfLsdbAdvertisement");
-            //SnmpWalker<VarbindCollection> walker = context.walk(1, "ospfLsdbAdvertisement");
-
-            VarbindCollection row = walker.next().get();
-            while (row != null) {
-                byte[] lsa = (byte[]) row.get("ospfLsdbAdvertisement").toObject();
-                    //System.out.println(Lsa.getInstance(lsa));
-                row = walker.next().get();
-                i++;
-            }
-            System.out.println("Count: " + i);
-        } catch (TimeoutException ex) {
-            System.err.println("SNMP connection timed out");
+        // Functions are handled differently from procedures. This is for the gds.util.asNode function.
+        // This function will give the following error if not unrestricted:
+        //
+        // gds.util.asNode is unavailable because it is sandboxed and has dependencies outside of the sandbox.
+        // Sandboxing is controlled by the dbms.security.procedures.unrestricted setting.
+        // Only unrestrict procedures you can trust with access to database internals.
+        Class[] functionsToRegister = { AsNodeFunc.class };
+        for (Class f : functionsToRegister) {
+            proceduresRegistry.registerFunction(f);
         }
+
+        List<Lsa> lsdb = snmpwalk();
+        lsdb.forEach(System.out::println);
 
         System.out.print("Press any key to exit...");
         System.in.read();
@@ -129,5 +104,36 @@ public class A3 {
 
     private enum RelTypes implements RelationshipType {
         LINKED
+    }
+
+    private static List<Lsa> snmpwalk() throws IOException {
+        Mib mib = MibFactory.getInstance().newMib();
+        mib.load("SNMPv2-MIB");
+        mib.load("IF-MIB");
+        mib.load("OSPF-MIB");
+
+        SecurityProtocols.getInstance().addAuthenticationProtocol(new AuthSHA());
+        SimpleSnmpV3Target target = new SimpleSnmpV3Target();
+        target.setAddress("192.168.0.1");
+        target.setSecurityName("admin");
+        target.setAuthType(SnmpV3Target.AuthType.SHA);
+        target.setPrivType(SnmpV3Target.PrivType.AES128);
+        target.setAuthPassphrase(System.getProperty("tnm4j.agent.auth.password", "cisco123"));
+        target.setPrivPassphrase(System.getProperty("tnm4j.agent.priv.password", "cisco123"));
+
+
+        List<Lsa> lsdb = new ArrayList<>();
+        try (SnmpContext context = SnmpFactory.getInstance().newContext(target, mib)) {
+            SnmpWalker<VarbindCollection> walker = context.walk(1, "sysName", "ospfLsdbAdvertisement");
+
+            VarbindCollection row = walker.next().get();
+            while (row != null) {
+                byte[] lsa = (byte[]) row.get("ospfLsdbAdvertisement").toObject();
+                lsdb.add(Lsa.getInstance(lsa));
+                //System.out.println(Lsa.getInstance(lsa));
+                row = walker.next().get();
+            }
+        }
+        return lsdb;
     }
 }
